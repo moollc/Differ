0ad5ca52076181bcb075504cbb7b0d04c43b261e
moollc <135183296+moollc@users.noreply.github.com>
2026-02-11 22:35:44 -0500

    Create sw.js

---
+++ sw.js
@@ -0,0 +1,27 @@
+const CACHE_NAME = 'differ-v3-cache-v1';
+const urlsToCache = [
+    './',
+    './Differ.html', // Or index.html if you rename it
+    'https://unpkg.com/react@18/umd/react.production.min.js',
+    'https://unpkg.com/react-dom@18/umd/react-dom.production.min.js',
+    'https://unpkg.com/babel-standalone@6/babel.min.js',
+    'https://cdn.tailwindcss.com',
+    'https://cdnjs.cloudflare.com/ajax/libs/font-awesome/6.0.0/css/all.min.css'
+];
+
+self.addEventListener('install', event => {
+    event.waitUntil(
+        caches.open(CACHE_NAME)
+            .then(cache => cache.addAll(urlsToCache))
+    );
+});
+
+self.addEventListener('fetch', event => {
+    event.respondWith(
+        caches.match(event.request)
+            .then(response => {
+                if (response) return response; // Return from cache
+                return fetch(event.request);   // Fetch from network
+            })
+    );
+});
